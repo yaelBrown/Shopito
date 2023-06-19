1fc4b14f205de57c6be7289a7aee62158b2f6217
yaelBrown <yaelrbrown@gmail.com>
2023-06-19 13:38:20 -0500

    chore: rem old style app, converting to nextjs

--- scripts/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-cd ../client
-git clone https://github.com/yaelBrown/shopito_client.git
-npm install
-
-cd ../server
-git clone https://github.com/yaelBrown/shopito_server.git
-npm install 
-cd ..
--- scripts/update.sh
+++
@@ -1,4 +0,0 @@
-cd ../client/shopito_client
-git pull
-cd ../../server/shopito_server
-git pull
